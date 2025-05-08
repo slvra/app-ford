59501a935ee06e706c130d6b254f0e8c47160d43
slvra <ss.adan@live.com>
2025-05-08 16:41:48 -0300

    teste commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-
 
 @Component({
   selector: 'app-root',
@@ -16,8 +11,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 export class AppComponent {
   title = 'app-ford';
   menuLinks = [
-    { label: 'Início', route: '/home' },
-    { label: 'Sobre', route: '/sobre' },
-    { label: 'Contato', route: '/contato' }
+    { label: 'home', route: '/home' },
+    { label: 'dashboard', route: '/dashboard' },
+    { label: 'login', route: '/login' },
   ];
 }
