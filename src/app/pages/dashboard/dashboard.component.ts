929781551cc01fab25a15d17bdf72e853583e5c2
slvra <ss.adan@live.com>
2025-05-08 14:51:45 -0300

    ajustado os cards no dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { CardComponent } from '../../components/card/card.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SidebarComponent, CardComponent],
+  imports: [SidebarComponent, CardComponent, ReactiveFormsModule],
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  vehicleList = new FormGroup({
+    
+    
+  })
+
 
 }
